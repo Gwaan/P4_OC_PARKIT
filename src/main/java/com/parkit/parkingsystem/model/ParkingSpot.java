4dd733e9b510c258e53772cdfbb21322f2c822b2
Gwenaël Boisnard <boisnard.gwenael@gmail.com>
2021-02-11 12:20:37 +0100

    Fix checkstyle reports for ParkinSpot class

--- src/main/java/com/parkit/parkingsystem/model/ParkingSpot.java
+++ src/main/java/com/parkit/parkingsystem/model/ParkingSpot.java
@@ -3,60 +3,114 @@ package com.parkit.parkingsystem.model;
 import com.parkit.parkingsystem.constants.ParkingType;
 
 /**
- * <p>Class representing a parking spot</p>
+ * <p>Class representing a parking spot.</p>
  *
  * @author Gwen
  * @version 1.0
  */
 public class ParkingSpot {
 
+    /**
+     * Parking slot.
+     */
     private int number;
+    /**
+     * Type of the parking spot.
+     */
     private ParkingType parkingType;
+    /**
+     * Availability of the parking spot.
+     */
     private boolean isAvailable;
 
     /**
-     * @param number      number of the parking slot
-     * @param parkingType type of vehicle
-     * @param isAvailable availability of the spot
+     * @param pNumber      number of the parking slot
+     * @param pParkingType type of vehicle
+     * @param pIsAvailable availability of the spot
      */
-    public ParkingSpot(int number, ParkingType parkingType, boolean isAvailable) {
-        this.number = number;
-        this.parkingType = parkingType;
-        this.isAvailable = isAvailable;
+    public ParkingSpot(final int pNumber, final ParkingType pParkingType, final boolean pIsAvailable) {
+        this.number = pNumber;
+        this.parkingType = pParkingType;
+        this.isAvailable = pIsAvailable;
     }
 
+    /**
+     * getter for parking slot number.
+     *
+     * @return parking slot number
+     */
     public int getId() {
         return number;
     }
 
-    public void setId(int number) {
-        this.number = number;
+    /**
+     * setter for parking slot number.
+     *
+     * @param pNumber parking number to set
+     */
+    public void setId(final int pNumber) {
+        this.number = pNumber;
     }
 
+    /**
+     * getter for DB driver.
+     *
+     * @return parking type
+     */
     public ParkingType getParkingType() {
         return parkingType;
     }
 
-    public void setParkingType(ParkingType parkingType) {
-        this.parkingType = parkingType;
+    /**
+     * setter for parking type.
+     *
+     * @param pParkingType vehicle type to be set
+     */
+    public void setParkingType(final ParkingType pParkingType) {
+        this.parkingType = pParkingType;
     }
 
+    /**
+     * getter for parking spot availability.
+     *
+     * @return either true if the parking sport is available or false if it's not
+     */
     public boolean isAvailable() {
         return isAvailable;
     }
 
-    public void setAvailable(boolean available) {
-        isAvailable = available;
+    /**
+     * setter for parking spot availability.
+     *
+     * @param pAvailable availability of the parking sport
+     */
+    public void setAvailable(final boolean pAvailable) {
+        isAvailable = pAvailable;
     }
 
+    /**
+     * Check equality of Parking Spot by ID.
+     *
+     * @param o Parking spot
+     * @return ID
+     */
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         ParkingSpot that = (ParkingSpot) o;
         return number == that.number;
     }
 
+    /**
+     * Define Hash with ID.
+     *
+     * @return ID
+     */
     @Override
     public int hashCode() {
         return number;
