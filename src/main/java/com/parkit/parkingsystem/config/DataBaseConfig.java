eb905852f392e4d70cdf0a44253e38555f4a5b57
Gwenaël Boisnard <boisnard.gwenael@gmail.com>
2021-02-11 11:29:58 +0100

    Fixed checkstyle report in DataBaseConfig and ReadProperties classes

--- src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
+++ src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
@@ -2,79 +2,89 @@ package com.parkit.parkingsystem.config;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
 
-import java.sql.*;
 
 /**
  * <p>Class allowing initialization and closure of a database connection
- * and closure of resources PreparedStatement and ResultSet</p>
+ * and closure of resources PreparedStatement and ResultSet.</p>
  *
  * @author Gwen
  * @version 1.0
  */
 public class DataBaseConfig {
 
-    private static final Logger logger = LogManager.getLogger("DataBaseConfig");
+    /**
+     * @see Logger
+     */
+    private static final Logger LOGGER = LogManager.getLogger("DataBaseConfig");
+    /**
+     * @see ReadProperties
+     */
     private ReadProperties rP = new ReadProperties();
 
     /**
-     * <p>Method returning a Mysql connection</p>
+     * <p>Method returning a Mysql connection.</p>
      *
      * @return connection to database
-     * @throws ClassNotFoundException if no definition for the class could be found
+     * @throws ClassNotFoundException f no definition for the class could be found
      * @throws SQLException           if a database access error occurs
      */
     public Connection getConnection() throws ClassNotFoundException, SQLException {
-        logger.info("Create DB connection");
+        LOGGER.info("Create DB connection");
         rP.getDbConnectionInfo();
         Class.forName(rP.getDriver());
         return DriverManager.getConnection(rP.getUrl(), rP.getUser(), rP.getPassword());
     }
 
     /**
-     * <p>Method closing a connection to database</p>
+     * <p>Method closing a connection to database.</p>
      *
      * @param con Connection to close
      */
-    public void closeConnection(Connection con) {
+    public void closeConnection(final Connection con) {
         if (con != null) {
             try {
                 con.close();
-                logger.info("Closing DB connection");
+                LOGGER.info("Closing DB connection");
             } catch (SQLException e) {
-                logger.error("Error while closing connection", e);
+                LOGGER.error("Error while closing connection", e);
             }
         }
     }
 
     /**
-     * <p>Method closing a PreparedStatement</p>
+     * <p>Method closing a PreparedStatement.</p>
      *
      * @param ps PreparedStatement to close
      */
-    public void closePreparedStatement(PreparedStatement ps) {
+    public void closePreparedStatement(final PreparedStatement ps) {
         if (ps != null) {
             try {
                 ps.close();
-                logger.info("Closing Prepared Statement");
+                LOGGER.info("Closing Prepared Statement");
             } catch (SQLException e) {
-                logger.error("Error while closing prepared statement", e);
+                LOGGER.error("Error while closing prepared statement", e);
             }
         }
     }
 
     /**
-     * <p>Method closing a ResultSet</p>
+     * <p>Method closing a ResultSet.</p>
      *
      * @param rs ResultSet to close
      */
-    public void closeResultSet(ResultSet rs) {
+    public void closeResultSet(final ResultSet rs) {
         if (rs != null) {
             try {
                 rs.close();
-                logger.info("Closing Result Set");
+                LOGGER.info("Closing Result Set");
             } catch (SQLException e) {
-                logger.error("Error while closing result set", e);
+                LOGGER.error("Error while closing result set", e);
             }
         }
     }
--- src/main/java/com/parkit/parkingsystem/config/ReadProperties.java
+++ src/main/java/com/parkit/parkingsystem/config/ReadProperties.java
@@ -3,23 +3,36 @@ package com.parkit.parkingsystem.config;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.*;
+import java.util.Properties;
+
 
 /**
- * <p>Class who provides info to establish a connection to database</p>
+ * <p>Class who provides info to establish a connection to database.</p>
  *
  * @author Gwen
  * @version 1.0
  */
 public class ReadProperties {
 
+    /**
+     * DB url.
+     */
     private String url;
+    /**
+     * DB user.
+     */
     private String user;
+    /**
+     * DB password.
+     */
     private String password;
+    /**
+     * DB driver.
+     */
     private String driver;
 
     /**
-     * <p>Method getting values from a properties file and setting them to fields</p>
+     * <p>Method getting values from a properties file and setting them to fields.</p>
      */
     public void getDbConnectionInfo() {
         FileInputStream fIS = null;
@@ -48,18 +61,38 @@ public class ReadProperties {
         }
     }
 
+    /**
+     * getter for DB url.
+     *
+     * @return DB's url
+     */
     public String getUrl() {
         return url;
     }
 
+    /**
+     * getter for DB user.
+     *
+     * @return DB's user
+     */
     public String getUser() {
         return user;
     }
 
+    /**
+     * getter for DB password.
+     *
+     * @return DB's password
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * getter for DB driver.
+     *
+     * @return DB's driver
+     */
     public String getDriver() {
         return driver;
     }
